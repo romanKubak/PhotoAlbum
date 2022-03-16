cac11a9d86af63975b5fe959849c4db3c080d63c
Vlad <woodygarryson@gmail.com>
2022-03-16 12:05:17 +0300

    somethisng

---
+++ routes/photos.js
@@ -0,0 +1,20 @@
+const router = require('express').Router();
+// const { Album, User, Photo, Admission } = require('../db/models/');
+
+
+router.get('/photo', async (req, res) => {
+  let allPhoto;
+  const idAlbum = ???; //Будем получать через fetch?
+  // console.log(author)
+  try {
+    allPhoto = await Photo.findAll({where: { album_id: idAlbum }}, {order:[['id', 'DESC']]}); 
+  } catch (error) {
+    return res.render('error', {
+      message: 'Не удалось получить записи из базы данных.',
+      error: {}
+    });
+  }
+  return res.render('photos/index', { allPhoto });
+});
+
+module.exports = router;
