dd6ebbbb760b3e795f8b0b23ac852a4ce3616afd
Roman <romakubak@gmail.com>
2022-03-16 21:15:42 +0300

    Отображение альбомов и ручка на конкретный альбом

--- middleware/common.js
+++ middleware/common.js
@@ -1,6 +1,6 @@
 exports.superUserName = (req, res, next) => {
   res.locals.superuser = req.session?.superuser;
   next();
-  // console.log('res.locals ===> ', res.locals)
+  // console.log('res.locals ===> ', res.locals.superuser)
   // console.log('req.session', req.session);
 };
--- routes/albums.js
+++ routes/albums.js
@@ -1,25 +1,10 @@
 const router = require('express').Router();
-const { Album, User, Photo, Admission } = require('../db/models/');
 
-router.get('/', async (req, res) => {
-  let authorAlbums;
-  let allAlbums;
-  const author = await User.findOne({where: {login: req.session.superuser}});
-  // console.log('author ===> ', author)
-  // const author = await User.findOne({where: {login: 'Loh'}});
+// Импортирую контролеры для работы роутеров
+const { showAlbums, photoOnAlbum } = require('../controlers/albumsControler');
 
-  const idAuthor = author.dataValues.id;
-  // console.log(author)
-  try {
-    authorAlbums = await Album.findAll({where: {user_id: idAuthor}}, {order:[['id', 'DESC']]});
-    console.log('authorAlbums ===> ', authorAlbums)
-    // проверить работоспособность !idAuthor
-    // allAlbums = await Album.findAll({where: { user_id: !idAuthor }}, {order:[['id', 'DESC']]}); 
-  } catch (error) {
-    res.render('error', { error: error.message });
-  }
-  // // console.log('entries Router', entries)
-  res.render('albums/index', { authorAlbums, allAlbums });
-});
+router.get('/', showAlbums);
+
+router.get('/album/:id', photoOnAlbum);
 
 module.exports = router;
