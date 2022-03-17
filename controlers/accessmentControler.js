e7f7c0d4fe133b9aba45ec2a579250d17f7d8d80
Roman <romakubak@gmail.com>
2022-03-17 21:40:38 +0300

    опять чтото починили

--- controlers/accessmentControler.js
+++ controlers/accessmentControler.js
@@ -8,17 +8,22 @@ async function showaccessAlbumId(req, res) {
     const idAuthor = author.dataValues.id;
     const allAdmission = await Admission.findAll()
     authorAlbums = await Album.findAll({ where: { user_id: idAuthor } }, { order: [['id', 'DESC']] });
-    console.log(allAdmission);
+    // console.log(allAdmission);
     if (allAdmission.length != 0) {
       const accessId = await Admission.findOne({ where: { user_id: idAuthor } })
-      const accessAlbumId = accessId.dataValues.album_id
-      try {
-        // authorAlbums = await Album.findAll({ where: { user_id: idAuthor } }, { order: [['id', 'DESC']] });
-        accessAlbum = await Album.findAll({ where: { id: accessAlbumId } })
-      } catch (error) {
-        res.render('error', { error: error.message });
+      if (accessId !== null) {
+        console.log('accessId ======> ',accessId)
+        const accessAlbumId = accessId.dataValues.album_id
+        try {
+          // authorAlbums = await Album.findAll({ where: { user_id: idAuthor } }, { order: [['id', 'DESC']] });
+          accessAlbum = await Album.findAll({ where: { id: accessAlbumId } })
+        } catch (error) {
+          res.render('error', { error: error.message });
+        }
+        return res.render('albums/index', { accessId, accessAlbumId, accessAlbum, authorAlbums });
+      } else {
+        return res.render('albums/index', {authorAlbums})
       }
-      return res.render('albums/index', { accessId, accessAlbumId, accessAlbum, authorAlbums });
     } else {
       return res.render('albums/index', {authorAlbums})
     }
